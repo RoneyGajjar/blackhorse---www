fb2d987fb47ddac311edb8111e54fda8d0526150
Roney Gajjar <roneygajjar121@gmail.com>
2026-05-28 13:50:30 -0700

    feat: add h prop to Situations component in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <Statement />
       <Features />
       <IndustriesSpecialize />
-      {/* <Situations /> */}
+      {/* <Situations h /> */}
       <Services />
       <Process />
       {/* <FAQ /> */}
